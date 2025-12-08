7f1ef3cccb7c36b5ad1890c5941a3cef7bc9c03a
NoumanAmir657 <noumanamir453@gmail.com>
2025-12-08 13:28:07 +0500

    Add Operation type so that we can have generic Op type
    Signed-off-by: NoumanAmir657 <noumanamir453@gmail.com>

--- concept-based-polymorphism/complex_opt.cpp
+++ concept-based-polymorphism/complex_opt.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <tuple>
 #include <stdexcept>
 #include <optional>
 
+struct Operation {
+  std::string name;
+  std::vector<int> operands;
+};
+
+template <typename Derived, typename... Traits> class Op {
+protected:
+  Operation* state;
+
+public:
+  explicit Op(Operation* state) : state(state) {}
+
+  Operation* getOperation() const { return state; }
+
+  template <typename T>
+  static constexpr bool has_trait = (std::is_same_v<T, Traits> || ...);
+};
+
 struct SideEffectsInterfaceTraits {
   struct Concept {
     virtual ~Concept() = default;
-    virtual bool hasSideEffect(void *op) const = 0;
+    virtual bool hasSideEffect(void *opaqueOp) const = 0;
   };
 
   template <typename ConcreteOp> struct Model : public Concept {
-    bool hasSideEffect(void *op) const override {
-      ConcreteOp *concreteOp = static_cast<ConcreteOp *>(op);
-      return concreteOp->hasSideEffect();
+    bool hasSideEffect(void *opaqueOp) const override {
+      Operation* opState = static_cast<Operation*>(opaqueOp);
+      ConcreteOp view(opState);
+      return view.hasSideEffect();
     }
   };
 };
@@ -41,60 +62,85 @@ public:
   bool hasSideEffect() const { return getImpl()->hasSideEffect(getEntity()); }
 };
 
-template <typename Derived, typename... Traits> class Op {
-public:
-  template <typename T>
-  static constexpr bool has_trait = (std::is_same_v<T, Traits> || ...);
-};
 
-class AddOp : public Op<AddOp> {
+class AddOp : public Op<AddOp, SideEffectsInterfaceTraits> {
 public:
+  using Op::Op;
+  static constexpr const char* name = "addOp";
+
+  int getLHS() const { return state->operands[0]; }
+  int getRHS() const { return state->operands[1]; }
+
   bool hasSideEffect() { return false; }
 };
 
 class LoadOp : public Op<LoadOp, SideEffectsInterfaceTraits> {
 public:
+  using Op::Op;
+  static constexpr const char* name = "loadOp";
+
+  int getAddress() const { return state->operands[0]; }
+
   bool hasSideEffect() { return true; }
 };
 
+
+template <typename ConcreteOp, typename... Args>
+ConcreteOp createOp(Args... args) {
+  Operation* op = new Operation();
+  op->name = ConcreteOp::name;
+
+  (op->operands.push_back(args), ...);
+
+  return ConcreteOp(op);
+}
+
 template <typename Interface, typename Derived, typename... Traits>
-Interface cast(Op<Derived, Traits...> *op) {
+Interface cast(Op<Derived, Traits...> op) {
   using RequiredTrait = typename Interface::Trait;
-  constexpr bool implements_trait = Op<Derived, Traits...>::template has_trait<RequiredTrait>;
+  constexpr bool has_trait = Op<Derived, Traits...>::template has_trait<RequiredTrait>;
 
-  static_assert(implements_trait, "\n\n ERROR: You are trying to cast an Op to an Interface it does not implement! \n\n");
+  static_assert(has_trait, "Op does not implement interface");
 
-  if constexpr (implements_trait) {
+  if constexpr (has_trait) {
     static typename RequiredTrait::template Model<Derived> static_model;
-    return Interface(op, &static_model);
+    return Interface(op.getOperation(), &static_model);
   } else {
-    std::terminate();
+    return Interface(nullptr, nullptr);
   }
 }
 
 template <typename Interface, typename Derived, typename... Traits>
-std::optional<Interface> dyn_cast(Op<Derived, Traits...> *op) {
+std::optional<Interface> dyn_cast(Op<Derived, Traits...> op) {
   using RequiredTrait = typename Interface::Trait;
 
   if constexpr (Op<Derived, Traits...>::template has_trait<RequiredTrait>) {
     static typename RequiredTrait::template Model<Derived> static_model;
-    return Interface(op, &static_model);
+    return Interface(op.getOperation(), &static_model);
   } else {
     return std::nullopt;
   }
 }
 
 int main() {
-  AddOp addOp;
-  LoadOp loadOp;
+  auto addOp = createOp<AddOp>(10, 20);
 
-  std::optional<SideEffectsInterface> addOpSideEffects = dyn_cast<SideEffectsInterface>(&addOp);
-  if (addOpSideEffects) {
-    std::cout << "Add operation has side effect = " << addOpSideEffects->hasSideEffect() << "\n";
-  }
+  auto loadOp = createOp<LoadOp>(123456789);
 
-  SideEffectsInterface loadOpSideEffects = cast<SideEffectsInterface>(&loadOp);
-  std::cout << "Load operation has side effect = " << loadOpSideEffects.hasSideEffect() << "\n";
+  std::cout << "=== Inspecting Data through Views ===\n";
+  std::cout << "AddOp: " << addOp.getLHS() << " + " << addOp.getRHS() << "\n";
+  std::cout << "LoadOp Addr: " << loadOp.getAddress() << "\n";
+
+  std::cout << "\n=== Using Interfaces ===\n";
+
+  SideEffectsInterface iface1 = cast<SideEffectsInterface>(addOp);
+  std::cout << "Add SideEffect: " << iface1.hasSideEffect() << "\n";
+
+  SideEffectsInterface iface2 = cast<SideEffectsInterface>(loadOp);
+  std::cout << "Load SideEffect: " << iface2.hasSideEffect() << "\n";
+
+  delete addOp.getOperation();
+  delete loadOp.getOperation();
 
   return 0;
 }
